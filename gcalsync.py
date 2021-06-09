92f67d66f39ac198c52457b12ad2c4ad388edf7a
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2021-06-09 12:49:43 +0200

    Make source calendar a command argument

--- gcalsync.py
+++ gcalsync.py
@@ -1,14 +1,15 @@
 from src.date_period import DatePeriod
 from src.calendar import Calendar
+import sys
 
-def main():
+def main(clientCalendarId):
     twCalendar = Calendar("tw", "primary")
     
-    clientCalendarId = "" ## Put here the ID of your client calendar visible from your TW account
-    ## use  twCalendar.printCalendars() to see all your calendars and find out this id. 
     clientCalendar = Calendar("tw", clientCalendarId )
 
     twCalendar.copyAllEventsFrom(clientCalendar, DatePeriod.weeks(1))
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
+    ## Pass as first an single argument the ID of your client calendar visible from your TW account
+    ## use  twCalendar.printCalendars() to see all your calendars and find out this id. 
