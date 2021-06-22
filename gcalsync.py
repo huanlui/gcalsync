534b160eac8261d7322057e212628bea8163c0d0
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2021-06-22 17:09:23 +0200

    Make source and target configurable

--- gcalsync.py
+++ gcalsync.py
@@ -2,14 +2,17 @@ from src.date_period import DatePeriod
 from src.calendar import Calendar
 import sys
 
-def main(clientCalendarId):
-    twCalendar = Calendar("tw", "primary")
-    
-    clientCalendar = Calendar("tw", clientCalendarId )
 
-    twCalendar.copyAllEventsFrom(clientCalendar, DatePeriod.weeks(1))
+def main(sourceAccountName, sourceCalendarId, targetAccountName, targetCalendarId):
+    sourceCalendar = Calendar(sourceAccountName, sourceCalendarId)
+    targetCalendar = Calendar(targetAccountName, targetCalendarId)
+
+    targetCalendar.copyAllEventsFrom(sourceCalendar, DatePeriod.weeks(1))
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    if len(sys.argv) < 4:
+        print(f"Usage python3 gcalsync sourceAccountName sourceCalendarId targetAccountName targetCalendarId")
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     ## Pass as first an single argument the ID of your client calendar visible from your TW account
-    ## use  twCalendar.printCalendars() to see all your calendars and find out this id. 
+    ## use  twCalendar.printCalendars() to see all your calendars and find out this id.
