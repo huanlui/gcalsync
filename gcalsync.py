632c244e26febcf7c2e28aa3bb3234ca42bae031
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2021-06-22 22:20:58 +0200

    Make date period configurable

--- gcalsync.py
+++ gcalsync.py
@@ -3,16 +3,20 @@ from src.calendar import Calendar
 import sys
 
 
-def main(sourceAccountName, sourceCalendarId, targetAccountName, targetCalendarId):
+def main(sourceAccountName, sourceCalendarId, targetAccountName, targetCalendarId, datePeriod):
+    print(f'Copying from {sourceAccountName}:{sourceCalendarId} to {targetAccountName}:{targetCalendarId}'
+          f' from {datePeriod.start} to {datePeriod.end}')
     sourceCalendar = Calendar(sourceAccountName, sourceCalendarId)
     targetCalendar = Calendar(targetAccountName, targetCalendarId)
 
     targetCalendar.copyAllEventsFrom(sourceCalendar, DatePeriod.weeks(1))
 
+    print('Events copied successfully')
+
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 5:
         print(f"Usage python3 gcalsync sourceAccountName sourceCalendarId targetAccountName targetCalendarId")
 
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4],DatePeriod.parse(sys.argv[5]))
     ## Pass as first an single argument the ID of your client calendar visible from your TW account
-    ## use  twCalendar.printCalendars() to see all your calendars and find out this id.
+    ## use  target.printCalendars() to see all your calendars and find out this id.
--- src/date_period.py
+++ src/date_period.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone, timedelta, time
+from datetime import datetime, timedelta
 from dataclasses import dataclass
 import pytz
 
@@ -15,8 +15,23 @@ class DatePeriod:
     def weeks(weeks):
         return DatePeriod(formatToUtc(datetime.utcnow()), formatToUtc(getEndOfWeek() + timedelta(weeks=weeks - 1)))
 
+    @staticmethod
+    def parse(input: str):
+        try:
+            if (input[-1] == 'd'):
+                return DatePeriod.days(int(input[:-1]))
+            if (input[-1] == 'w'):
+                return DatePeriod.weeks(int(input[:-1]))
+            else:
+                raise Exception()
+        except:
+            raise Exception(
+                f'DatePeriod "{input}" not valid. Valid examples: 2d = 2 days from today, 3w: 3 days from today')
+
+
 def formatToUtc(datetime):
-    return datetime.isoformat().replace('+00:00','') + 'Z'
+    return datetime.isoformat().replace('+00:00', '') + 'Z'
+
 
 def getEndOfDay():
     today = datetime.utcnow()
@@ -25,8 +40,9 @@ def getEndOfDay():
     end = start + timedelta(days=1) - timedelta(microseconds=1)
     return end
 
+
 def getEndOfWeek():
-    end_of_day = getEndOfDay() 
+    end_of_day = getEndOfDay()
     start = end_of_day - timedelta(days=end_of_day.weekday())
     end = start + timedelta(days=6) - timedelta(seconds=1)
     return end
